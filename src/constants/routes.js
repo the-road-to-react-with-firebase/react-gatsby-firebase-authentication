703ab6c91fd5e0bd705ca001cab94f897c6f8bae
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-29 15:15:54 +0200

    Sign In, Account & Home are client side routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
-export const PASSWORD_FORGET = '/pw-forget';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/app/pw-forget';
+export const HOME = '/app/home';
+export const ACCOUNT = '/app/account';
