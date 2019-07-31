abf31423d14e6c7026c4dc3543d4f3f966b84b7e
Belciug Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2019-08-01 00:16:55 +0300

    PasswordForget - Use destructured variable in render

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -44,7 +44,7 @@ class PasswordForgetForm extends Component {
       <form onSubmit={this.onSubmit}>
         <input
           name="email"
-          value={this.state.email}
+          value={email}
           onChange={this.onChange}
           type="text"
           placeholder="Email Address"
